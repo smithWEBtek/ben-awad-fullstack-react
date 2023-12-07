c606fda8970f58388eac6c4687421c739aaa3523
brad@smithwebtek.com <brad@smithwebtek.com>
2023-12-07 07:07:47 -0500

    GraphQL working

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,17 +1,45 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+// import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+
+  // app.get('/posts', (_, res) => {
+  //   res.send(posts.map((post) => post.title))
+  // })
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.get('/', (_, res) => {
+    res.send("hello there Frank");
+  })
+
+  app.listen(4000, () => {
+    console.log('***  server started on localhost:4000  ***');
+  })
 };
 
 main().catch((err) => {
   console.error(err);
 });
+
+
+
